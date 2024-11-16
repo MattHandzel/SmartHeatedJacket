cc04d6484cd448d700599489a47d46e2c7514332
Wavydavid64 <davidevanhoward64@gmail.com>
2024-11-15 18:27:52 -0600

    temperature:

--- Firmware/heated_fabric_driver.py
+++ Firmware/heated_fabric_driver.py
@@ -1,2 +1,12 @@
+# Credit:
+# AdaFruit Conductive Heater Fabric Tutorial: https://learn.adafruit.com/experimenting-with-conductive-heater-fabric/how-do-i-power-it
+
+from digitalio import DigitalInOut, Direction
+import board
+
+# Set up the transistor on Pin 10, name it "base", and make it an output.
+base = DigitalInOut(board.D10)
+base.direction = Direction.OUTPUT
+
 def heat_fabric(intensity):
     pass
